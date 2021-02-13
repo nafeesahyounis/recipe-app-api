47989a0cbd4a9a547648815be804f5aee918c627
nafeesahyounis <nafeesah.youniss@gmail.com>
2021-02-13 17:28:03 +0100

    wrote practise unit tests for calc function to practice tdd

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+from app.calc import add, subtract
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        self.assertEqual(add(3,8),11)
+
+    def test_subtract_numbers(self):
+        """Test that two number can be subtracted"""
+        self.assertEqual(subtract(11,5), 6)
